6c0760ca1b687533ead546f25e1d9f2d49e322f5
Remy Ticona <realtica@gmail.com>
2021-09-15 11:33:46 -0500

    i3-gaps

--- i3-gaps.sh
+++ i3-gaps.sh
@@ -1,34 +1,21 @@
 #!/bin/bash
 
-# Installing i3-gaps
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'Installing i3-gaps'
+echo 'Installing ninja build system'
 
+sudo apt-get install meson ninja-build
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
 ## Dependencies
-# i3-gaps has some packages that are required for it to work so install these things:
-sudo apt install libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf xutils-dev libtool automake
-# You also need to install `libxcb-xrm-dev`, but I got `Unable to locate package libxcb-xrm-dev` when trying to install from the apt repositories on Ubuntu 16.04. If this happens to you, just install it from source using these commands:
-mkdir tmp
-cd tmp
-git clone https://github.com/Airblader/xcb-util-xrm
-cd xcb-util-xrm
-git submodule update --init
-./autogen.sh --prefix=/usr
-make
-sudo make install
-
-## Installing
-
-#gaps also needs to be installed from source so run these commands:
-cd tmp
+echo 'i3-gaps has some packages that are required for it to work so install these things'
+sudo apt install dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev
+echo '+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++'
+echo 'gaps also needs to be installed from source so run these commands:'
 git clone https://www.github.com/Airblader/i3 i3-gaps
 cd i3-gaps
-git checkout gaps && git pull
-autoreconf --force --install
-rm -rf build
-mkdir build
-cd build
-../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
-make
-sudo make install
+mkdir -p build && cd build
+meson ..
+ninja
 #Now i3-gaps should be installed.
 
 ## Configuring
